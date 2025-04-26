43710ab56c8539d01099a4ad1fc72c0a9d293802
NimohK <catenimowambu@gmail.com>
2025-04-26 13:57:48 +0300

    My first greeting app

---
+++ greeting .py
@@ -0,0 +1,7 @@
+# Personalized greeting app
+# Ask for user name
+name = input("What is your name?")
+# Ask for users's favourite color
+color = input("What is your favourite color?")
+
+print(f"Hello {name}! Your favourite color is {color}")
